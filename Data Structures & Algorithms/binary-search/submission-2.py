98138c51c94d548a567c7694bffa78086ab609ec
TheCaister <edisoncai2001@gmail.com>
2026-07-13 22:27:28 +0100

    Add: binary-search - submission-2

---
+++ Data Structures & Algorithms/binary-search/submission-2.py
@@ -0,0 +1,18 @@
+# if when getting mid we round down, (r - l // 2)
+# when updating window let's move left to mid + 1
+
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        l, r = 0, len(nums) - 1
+
+        while l <= r:
+            mid = l + ((r - l) // 2)
+            mid_num = nums[mid]
+            if mid_num == target:
+                return mid
+            elif mid_num < target:
+                l = mid + 1
+            else:
+                r = mid - 1
+
+        return -1
