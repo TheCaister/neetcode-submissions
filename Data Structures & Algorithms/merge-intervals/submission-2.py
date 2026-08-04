a316dbff2c23a3753f9cf3044510333162b35ad6
TheCaister <edisoncai2001@gmail.com>
2026-08-05 00:33:09 +0100

    Add: merge-intervals - submission-2

---
+++ Data Structures & Algorithms/merge-intervals/submission-2.py
@@ -0,0 +1,32 @@
+# a stack might be useful here
+# keep adding
+# if overlapping, pop and add new one
+
+class Solution:
+    def merge(self, intervals: List[List[int]]) -> List[List[int]]:
+        res = []
+
+        intervals.sort()
+
+        for interval in intervals:
+            if res:
+                latestInterval = res[-1]
+
+                print(latestInterval)
+                print(interval)
+
+                if (interval[1] <= latestInterval[1] or
+                interval[0] <= latestInterval[1]
+                ):
+                    newInterval = [
+                        min(interval[0], latestInterval[0]),
+                        max(interval[1], latestInterval[1])
+                    ]
+                    res.pop()
+                    res.append(newInterval)
+                else:
+                    res.append(interval)
+            else:
+                res.append(interval)
+
+        return res
