d258e0e1397af089904d28fb14de34e608448099
TheCaister <edisoncai2001@gmail.com>
2026-08-05 20:14:36 +0100

    Add: jump-game - submission-3

---
+++ Data Structures & Algorithms/jump-game/submission-3.py
@@ -0,0 +1,11 @@
+# brute force - try each start point and see if you cna reach the end
+
+class Solution:
+    def canJump(self, nums: List[int]) -> bool:
+        goal = len(nums) - 1
+
+        for i in range(len(nums) - 1, -1, -1):
+            if i + nums[i] >= goal:
+                goal = i
+        
+        return True if goal == 0 else False
