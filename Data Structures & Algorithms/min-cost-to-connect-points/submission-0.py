c38d64764f557c9b6b4f1544d5bde72f47c7060c
TheCaister <edisoncai2001@gmail.com>
2026-08-09 21:08:38 +0100

    Add: min-cost-to-connect-points - submission-0

---
+++ Data Structures & Algorithms/min-cost-to-connect-points/submission-0.py
@@ -0,0 +1,28 @@
+# brute force all possible non-cyclical combinations
+# for each point, can you just greedily try to take the closest one, and mark it as visited so future points won't consider it? so n^2
+
+class Solution:
+    def minCostConnectPoints(self, points: List[List[int]]) -> int:
+        N = len(points)
+        adj = {i: [] for i in range(N)}
+        for i in range(N):
+            x1, y1 = points[i]
+            for j in range(i + 1, N):
+                x2, y2 = points[j]
+                dist = abs(x1 - x2) + abs(y1 - y2)
+                adj[i].append([dist, j])
+                adj[j].append([dist, i])
+
+        res = 0
+        visit = set()
+        minH = [[0, 0]]
+        while len(visit) < N:
+            cost, i = heapq.heappop(minH)
+            if i in visit:
+                continue
+            res += cost
+            visit.add(i)
+            for neiCost, nei in adj[i]:
+                if nei not in visit:
+                    heapq.heappush(minH, [neiCost, nei])
+        return res
