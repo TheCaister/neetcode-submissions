6d7889f062e4eb4a56cfec1a20841d328b66f71d
TheCaister <edisoncai2001@gmail.com>
2026-08-11 22:25:14 +0100

    Add: climbing-stairs - submission-1

---
+++ Data Structures & Algorithms/climbing-stairs/submission-1.py
@@ -0,0 +1,22 @@
+# at n, since you can take 2 steps, there are x ways to get to the n - 2 step, and y ways to get to the n - 1 step.
+# you just need to add up x and y
+# as long as we have the first 2 to start off from, we can start building it up
+# quick solution, build up a list of size n, get the last element
+# but tbh we should only need 3 vars?
+# for 1, there's only 1 way. for 2, there are 2 ways, we can start from there
+# on each step, x = y, and y becomes the new result
+
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        x = 1
+        y = 2
+
+        if n == 1:
+            return 1
+
+        for i in range(3, n + 1):
+            tmp = x + y
+            x = y
+            y = tmp
+
+        return y
